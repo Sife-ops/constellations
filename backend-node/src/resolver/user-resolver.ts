ad09fa560787ca3be7a9ab661e6d9ae4974f47b9
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-01-21 15:37:54 -0800

    token functions

--- backend-node/src/resolver/user-resolver.ts
+++ backend-node/src/resolver/user-resolver.ts
@@ -1,7 +1,7 @@
 import argon2 from "argon2";
 import jwt from "jsonwebtoken";
 import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
-import { Request, Response } from "express";
+import { CookieOptions, Request, Response } from "express";
 import { User } from "../entities/user";
 
 interface AuthContext {
@@ -14,45 +14,6 @@ export class UserResolver {
   @Query(() => String)
   helloWorld() {
     return "hello";
-  }
-
-  @Mutation(() => String)
-  async login(
-    @Arg("email", () => String) email: string,
-    @Arg("password", () => String) password: string,
-    @Ctx() { req, res }: AuthContext
-  ): Promise<string> {
-    console.log("request cookies:", req.cookies)
-
-    const user = await User.findOne({
-      where: {
-        email,
-      },
-    });
-    if (!user) {
-      throw new Error("user does not exist");
-    }
-
-    const verified = await argon2.verify(user.password, password);
-    if (!verified) {
-      throw new Error("incorrect password");
-    }
-
-    // todo: token functions
-    const refreshToken = jwt.sign({ id: user.id }, "refresh", {
-      expiresIn: 6000,
-    });
-    res.cookie("wg", refreshToken, {
-      httpOnly: true,
-      sameSite: "none",
-      secure: true,
-    });
-
-    const accessToken = jwt.sign({ id: user.id }, "access", {
-      expiresIn: 1500,
-    });
-
-    return accessToken;
   }
 
   @Mutation(() => User)
@@ -78,10 +39,94 @@ export class UserResolver {
         email,
       },
     });
+
     if (!user) {
       throw new Error("failed to create user");
     }
 
     return user;
   }
+
+  @Mutation(() => String)
+  async login(
+    @Arg("email", () => String) email: string,
+    @Arg("password", () => String) password: string,
+    @Ctx() { req, res }: AuthContext
+  ): Promise<string> {
+    console.log("request cookies:", req.cookies);
+
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    });
+    if (!user) {
+      throw new Error("user does not exist");
+    }
+
+    const verified = await argon2.verify(user.password, password);
+    if (!verified) {
+      throw new Error("incorrect password");
+    }
+
+    sendRefreshToken(res, { id: user.id });
+
+    return newAccessToken({ id: user.id });
+  }
+
+  // todo: query or mutation?
+  @Mutation(() => Boolean)
+  async userExists(
+    @Arg("email", () => String, { nullable: true }) email?: string,
+    @Arg("username", () => String, { nullable: true }) username?: string
+  ): Promise<boolean> {
+    if (!username && !email) {
+      throw new Error("invalid query args");
+    }
+
+    const fieldIs = () => {
+      if (username) return { username };
+      return { email };
+    };
+
+    const user = await User.findOne({
+      where: fieldIs(),
+    });
+
+    if (!user) return false;
+
+    return true;
+  }
 }
+
+const newRefreshToken = (payload: { id: number }): string => {
+  return jwt.sign(
+    //
+    payload,
+    "refresh",
+    {
+      expiresIn: 6000,
+    }
+  );
+};
+
+const newAccessToken = (payload: { id: number }): string => {
+  return jwt.sign(
+    //
+    payload,
+    "access",
+    {
+      expiresIn: 1500,
+    }
+  );
+};
+
+const cookieOptions: CookieOptions = {
+  httpOnly: true,
+  sameSite: "none",
+  secure: true,
+};
+
+const sendRefreshToken = (res: Response, payload: { id: number }) => {
+  res.cookie("wg", newRefreshToken(payload), cookieOptions);
+};
