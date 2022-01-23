d8dcb7c468e95260ca1873f0a3b611c710aa7408
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-01-22 21:04:28 -0800

    auth config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,12 +2,39 @@ import "./index.css";
 import App from "./App";
 import React from "react";
 import ReactDOM from "react-dom";
-import { createClient, Provider as UrqlProvider } from "urql";
-import { getAccessToken } from "./utility/token";
 import decode, { JwtPayload } from "jwt-decode";
+import { authExchange, AuthConfig } from "@urql/exchange-auth";
+import { getAccessToken } from "./utility/token";
+
+import {
+  //
+  createClient,
+  Provider as UrqlProvider,
+} from "urql";
+
+const authConfig: AuthConfig<{ accessToken: string }> = {
+  getAuth: async ({ authState }) => {
+    if (!authState) {
+      const accessToken = getAccessToken();
+      if (accessToken) return { accessToken };
+      return null;
+    }
+
+    const res = await fetch("http://localhost:4000/refresh", {
+      method: "POST",
+      credentials: "include",
+    });
+
+    const data = await res.json();
+    const accessToken = data.accessToken as string;
+  },
+};
 
 const client = createClient({
   url: "http://localhost:4000/graphql",
+
+  // exchanges: [authExchange(authConfig)],
+
   fetchOptions: () => {
     const token = getAccessToken();
     if (token) {
