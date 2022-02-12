441da19c92dc5491008eeea85700523b735adb26
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-02-11 21:28:58 -0800

    reset refactor

--- frontend/src/component/reset.tsx
+++ frontend/src/component/reset.tsx
@@ -1,85 +1,33 @@
 import React from 'react';
-import EmailIcon from '@mui/icons-material/Email';
-
-import {
-  Avatar,
-  Box,
-  Button,
-  Container,
-  CssBaseline,
-  Grid,
-  Link,
-  TextField,
-  Typography,
-} from '@mui/material';
+import { Formik } from 'formik';
+import { Link } from 'react-router-dom';
 
 export const Reset: React.FC = () => {
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-          <EmailIcon />
-        </Avatar>
-
-        <Typography component="h1" variant="h5">
-          Reset password
-        </Typography>
-
-        <Typography component="p" variant="subtitle1">
-          Send an email with a link to reset your password.
-        </Typography>
-
-        <Box
-          component="form"
-          // onSubmit={handleSubmit}
-          noValidate
-          sx={{ mt: 1 }}
-        >
-          <TextField
-            margin="normal"
-            required
-            fullWidth
-            id="email"
-            className="auto-login__emailInput"
-            label="Email Address"
+    <Formik
+      initialValues={{ email: '' }}
+      onSubmit={async ({ email }, { setSubmitting }) => {
+        setSubmitting(true);
+      }}
+    >
+      {({ handleChange, handleSubmit, isSubmitting, values }) => (
+        <form onSubmit={handleSubmit}>
+          <input
             name="email"
-            autoComplete="email"
-            autoFocus
-            // value={email}
-            // onChange={(e) => setEmail(e.target.value)}
+            onChange={handleChange}
+            placeholder="email"
+            value={values.email}
           />
-
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            className="auto-login__submit"
-            sx={{ mt: 3, mb: 2 }}
-          >
-            Reset
-          </Button>
-
-          <Grid container>
-            <Grid item>
-              <Link
-                className="auto-login__registerLink"
-                href="/login"
-                variant="body2"
-              >
-                {'Already have an account? Sign In'}
-              </Link>
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
-    </Container>
+          <br />
+          <button disabled={isSubmitting} type="submit">
+            submit
+          </button>
+          <br />
+          <Link to="/login">{'Already have an account? Sign In'}</Link>
+          <br />
+          <Link to="/register">{"Don't have an account? Sign Up"}</Link>
+        </form>
+      )}
+    </Formik>
   );
 };
