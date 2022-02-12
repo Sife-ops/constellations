7b7dc52b592aed111b569644d35f533defe1178f
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-02-11 20:59:05 -0800

    login refactor

--- frontend/src/component/login.tsx
+++ frontend/src/component/login.tsx
@@ -1,23 +1,7 @@
-import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import React from 'react';
 import { Formik } from 'formik';
-import { ex } from '../utility/constant';
+import { Link, useNavigate } from 'react-router-dom';
 import { useLoginMutation } from '../generated/graphql';
-import { useNavigate } from 'react-router-dom';
-
-import {
-  Avatar,
-  Box,
-  Button,
-  Checkbox,
-  Container,
-  CssBaseline,
-  FormControlLabel,
-  Grid,
-  Link,
-  TextField,
-  Typography,
-} from '@mui/material';
 
 export const Login: React.FC = () => {
   const navigate = useNavigate();
@@ -25,143 +9,60 @@ export const Login: React.FC = () => {
   const [loginResult, loginMutation] = useLoginMutation();
 
   return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-        }}
-      >
-        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-          <LockOutlinedIcon />
-        </Avatar>
-
-        <Typography component="h1" variant="h5">
-          Sign in
-        </Typography>
-
-        <Formik
-          initialValues={{ email: '', password: '', remember: false }}
-          validate={(values) => {
-            const errors: { email?: string } = {};
-            const emailError = (s: string): string | undefined => {
-              if (s === '') return undefined;
-              const isValid = ex.email.test(s.toLowerCase());
-              if (!isValid) return 'invalid email address';
-              return undefined;
-            };
-            const email = emailError(values.email);
-            if (email) errors.email = email;
-            return errors;
-          }}
-          onSubmit={async (
-            { email, password, remember },
-            { setSubmitting }
-          ) => {
-            setSubmitting(true);
-            const res = await loginMutation({
-              email,
-              password,
-              remember,
-            });
-            if (res.error) {
-              return navigate('/reset');
-            }
-            window.location.reload();
-          }}
-        >
-          {({
-            //
-            errors,
-            handleChange,
-            handleSubmit,
-            isSubmitting,
-            setFieldValue,
-            values,
-          }) => (
-            <Box
-              component="form"
-              onSubmit={handleSubmit}
-              noValidate
-              sx={{ mt: 1 }}
-            >
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                id="email"
-                className="auto-login__emailInput"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-                autoFocus
-                value={values.email}
-                onChange={handleChange}
-                error={errors.email !== '' && errors.email !== undefined}
-              />
-
-              <TextField
-                margin="normal"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                className="auto-login__passwordInput"
-                autoComplete="current-password"
-                value={values.password}
-                onChange={handleChange}
-                // error={}
-                // helperText={}
-              />
-
-              <FormControlLabel
-                control={
-                  <Checkbox
-                    value="remember"
-                    color="primary"
-                    checked={values.remember}
-                    onChange={() => setFieldValue('remember', !values.remember)}
-                  />
-                }
-                label="Remember me"
-              />
-
-              <Button
-                type="submit"
-                fullWidth
-                variant="contained"
-                className="auto-login__submit"
-                sx={{ mt: 3, mb: 2 }}
-                disabled={isSubmitting}
-              >
-                Sign In
-              </Button>
-
-              <Grid container>
-                <Grid item xs>
-                  <Link href="/reset" variant="body2">
-                    Forgot password?
-                  </Link>
-                </Grid>
-                <Grid item>
-                  <Link
-                    className="auto-login__registerLink"
-                    href="/register"
-                    variant="body2"
-                  >
-                    {"Don't have an account? Sign Up"}
-                  </Link>
-                </Grid>
-              </Grid>
-            </Box>
-          )}
-        </Formik>
-      </Box>
-    </Container>
+    <Formik
+      initialValues={{ email: '', password: '', remember: false }}
+      onSubmit={async ({ email, password, remember }, { setSubmitting }) => {
+        setSubmitting(true);
+        const res = await loginMutation({
+          email,
+          password,
+          remember,
+        });
+        if (res.error) {
+          return navigate('/reset');
+        }
+        window.location.reload();
+      }}
+    >
+      {({
+        handleChange,
+        handleSubmit,
+        isSubmitting,
+        setFieldValue,
+        values,
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <input
+            name="email"
+            onChange={handleChange}
+            placeholder="email"
+            value={values.email}
+          />
+          <br />
+          <input
+            name="password"
+            onChange={handleChange}
+            placeholder="password"
+            type="password"
+            value={values.password}
+          />
+          <br />
+          <input
+            type="checkbox"
+            checked={values.remember}
+            onChange={() => setFieldValue('remember', !values.remember)}
+          />
+          <label>Remember me</label>
+          <br />
+          <button disabled={isSubmitting} type="submit">
+            submit
+          </button>
+          <br />
+          <Link to="/reset">Forgot password?</Link>
+          <br />
+          <Link to="/register">{"Don't have an account? Sign Up"}</Link>
+        </form>
+      )}
+    </Formik>
   );
 };
--- frontend/src/component/register.tsx
+++ frontend/src/component/register.tsx
@@ -1,180 +1,58 @@
 import React from 'react';
 import _ from 'lodash';
+import { Formik } from 'formik';
 import { Link, useNavigate } from 'react-router-dom';
-
-import {
-  useRegisterMutation,
-  useUserExistsMutation,
-} from '../generated/graphql';
-
-import {
-  emailIsValid,
-  passwordIsValid,
-  usernameIsValid,
-} from '../utility/function';
-
-// todo: use union type
-enum Tristate {
-  true = 'true',
-  false = 'false',
-  default = 'default',
-}
-
-type UserExistsVariable = 'email' | 'username';
-
-type InputError = [boolean, string];
+import { useRegisterMutation } from '../generated/graphql';
 
 export const Register: React.FC = () => {
   const navigate = useNavigate();
 
-  const [userExistsResult, userExistsMutation] = useUserExistsMutation();
   const [registerResult, registerMutation] = useRegisterMutation();
 
-  const [email, setEmail] = React.useState('');
-  const [username, setUsername] = React.useState('');
-  const [password, setPassword] = React.useState('');
-
-  // todo: change to emailAvailable?
-  const [emailExists, setEmailExists] = React.useState<Tristate>(
-    Tristate.default
-  );
-  const [usernameExists, setUsernameExists] = React.useState<Tristate>(
-    Tristate.default
-  );
-
-  const [registerSuccess, setRegisterSuccess] = React.useState<boolean>(false);
-
-  const debounceInputCb = (
-    variable: UserExistsVariable,
-    setInputExists: React.Dispatch<React.SetStateAction<Tristate>>
-  ) => {
-    return async (nextValue: string) => {
-      const res = await userExistsMutation({ [variable]: nextValue });
-      if (res.data?.userExists) {
-        setInputExists(Tristate.true);
-        return;
-      }
-      setInputExists(Tristate.false);
-    };
-  };
-
-  const debounceEmail = React.useRef(
-    _.debounce(debounceInputCb('email', setEmailExists), 1000)
-  ).current;
-
-  const debounceUsername = React.useRef(
-    _.debounce(debounceInputCb('username', setUsernameExists), 1000)
-  ).current;
-
-  // todo: use event types
-  const handleEmail = (e: any) => {
-    setEmailExists(Tristate.default);
-    setEmail(e.target.value);
-    debounceEmail(e.target.value);
-  };
-
-  const handleUsername = (e: any) => {
-    setUsernameExists(Tristate.default);
-    setUsername(e.target.value);
-    debounceUsername(e.target.value);
-  };
-
-  const handleSubmit = async (e: any) => {
-    e.preventDefault();
-    const res = await registerMutation({ email, password, username });
-    if (res.error) return;
-    setRegisterSuccess(true);
-    setTimeout(() => {
-      navigate('login');
-    }, 2000);
-  };
-
-  const emailError = (): InputError => {
-    if (email !== '' && !emailIsValid(email)) {
-      return [true, 'invalid email'];
-    }
-    if (emailExists === Tristate.true) {
-      return [true, 'already registered'];
-    }
-    return [false, ''];
-  };
-
-  const usernameError = (): InputError => {
-    if (username !== '' && !usernameIsValid(username)) {
-      return [true, '5-15 characters, letters and number only'];
-    }
-    if (usernameExists === Tristate.true) {
-      return [true, 'username unavailable'];
-    }
-    return [false, ''];
-  };
-
-  const passwordError = (): InputError => {
-    if (password !== '' && !passwordIsValid(password)) {
-      return [
-        true,
-        'Use at least 8 characters, one number, and one special character.',
-      ];
-    }
-    return [false, ''];
-  };
-
-  const [emailIsError, emailHelperText] = emailError();
-  const [usernameIsError, usernameHelperText] = usernameError();
-  const [passwordIsError, passwordHelperText] = passwordError();
-
-  const emailColor = () => {
-    if (email !== '' && emailExists === Tristate.false) return 'success';
-    return 'primary';
-  };
-
-  const usernameColor = () => {
-    if (username !== '' && usernameExists === Tristate.false) return 'success';
-    return 'primary';
-  };
-
-  const passwordColor = () => {
-    if (password !== '' && passwordIsValid(password)) return 'success';
-    return 'primary';
-  };
-
-  const registerDisabled = (): boolean => {
-    if (emailExists !== Tristate.false || !emailIsValid(email)) {
-      return true;
-    }
-    if (usernameExists !== Tristate.false || !usernameIsValid(username)) {
-      return true;
-    }
-    if (!passwordIsValid(password)) return true;
-    return false;
-  };
-
   return (
-    <form onSubmit={handleSubmit}>
-      <input
-        //
-        onChange={handleEmail}
-        placeholder="email"
-        value={email}
-      />
-      <br />
-      <input
-        onChange={handleUsername}
-        placeholder="username"
-        value={username}
-      />
-      <br />
-      <input
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="password"
-        value={password}
-      />
-      <br />
-      <button type="submit">submit</button>
-      <br />
-      <Link to="/reset">Forgot password?</Link>
-      <br />
-      <Link to="/login">{'Already have an account? Sign In'}</Link>
-    </form>
+    <Formik
+      initialValues={{ email: '', username: '', password: '' }}
+      onSubmit={async ({ email, username, password }, { setSubmitting }) => {
+        setSubmitting(true);
+        const res = await registerMutation({ email, password, username });
+        if (res.error) return;
+        navigate('login');
+      }}
+    >
+      {({ handleChange, handleSubmit, isSubmitting, values }) => (
+        <form onSubmit={handleSubmit}>
+          <input
+            //
+            name="email"
+            onChange={handleChange}
+            placeholder="email"
+            value={values.email}
+          />
+          <br />
+          <input
+            name="username"
+            onChange={handleChange}
+            placeholder="username"
+            value={values.username}
+          />
+          <br />
+          <input
+            name="password"
+            onChange={handleChange}
+            placeholder="password"
+            type="password"
+            value={values.password}
+          />
+          <br />
+          <button disabled={isSubmitting} type="submit">
+            submit
+          </button>
+          <br />
+          <Link to="/reset">Forgot password?</Link>
+          <br />
+          <Link to="/login">{'Already have an account? Sign In'}</Link>
+        </form>
+      )}
+    </Formik>
   );
 };
