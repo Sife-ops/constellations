a11652b25427b8f33cfcf8895cb7f995c80d02af
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-02-22 19:34:38 -0800

    move files

--- backend/docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-docker run \
-    --mount type=bind,source="$(pwd)"/database,target=/app/database \
-    --name constellations_backend \
-    --rm \
-    -d \
-    -p 4000:80 \
-    constellations_backend
-
--- frontend/docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-docker run \
-    --name constellations_frontend \
-    --rm \
-    -d \
-    -p 3001:80 \
-    constellations_frontend
