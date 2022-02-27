9273d092c7ef16b247db1d4bff8bdabfce755989
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2022-02-27 14:35:02 -0800

    move scripts to deploy repo

--- deploy/ngrok-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-# to run with ngrok, define url before running this
-# export VITE_NGROK_URL="your ngrok url"
-
-pushd ./frontend
-npm run build
-popd
-docker-compose -f docker-compose-ngrok.yml up --build
--- deploy/vpc-start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-. ./env.sh
-
-cp ./docker-compose-vpc.yml ../docker-compose.yml
-
-pushd ../
-
-docker-compose up $@
